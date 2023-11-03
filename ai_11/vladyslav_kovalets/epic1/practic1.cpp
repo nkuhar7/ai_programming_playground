c490929ffeee57e3dfb1017ecf17d3534bc81810
kovalets-vlad <vladyslav.kovalets.shi.2023@gmail.com>
2023-11-03 11:50:35 +0200

    add practic

--- ai_11/vladyslav_kovalets/epic1/practic1.cpp
+++ ai_11/vladyslav_kovalets/epic1/practic1.cpp
@@ -7,24 +7,22 @@ int main()
     double vnesok;
     double time;
     double rate, suma;
-    cout << "Please enter deposit ";
-    cin >> vnesok;
+    printf("Please enter deposit: ");
+    scanf_s("%lf", &vnesok);
     suma = vnesok;
-    cout << "Please enter years ";
-    cin >> time;
-    cout << "Please enter rate ";
-    cin >> rate;
+    printf("Please enter years: ");
+    scanf_s("%lf", &time);
+    printf("Please enter rate: ");
+    scanf_s("%lf", &rate);
     string name;
-    cout << "Enter your name ";
+    printf("Enter your name: ");
     cin >> name;
-    for(int i = 0; i< time; i++)
+
+    for (int i = 0; i < time; i++)
     {
-        suma = suma + (suma/100 * rate );
+        suma = suma + (suma / 100 * rate);
     }
-    printf ("Hi %s your summa = %f", name.c_str(), suma); 
-
-
-
+    printf("Hi %s, your summa = %f", name.c_str(), suma);
 
     return 0;
 }
