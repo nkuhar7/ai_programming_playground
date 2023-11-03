8bdcecff17e4668279fa1399c0407664d81d8426
Yulia Salishcheva <Yuliia.Salishcheva.ShI.2023@lpnu.ua>
2023-11-03 11:19:09 +0200

    Added files for epic_1

---
+++ ai_11/yuliia_salishcheva/epic_1/AplusB.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int main()
+{
+    short int a, b;
+    std::cin >> a;
+    std::cin >> b;
+    std::cout << a + b;
+}
+
+
+
+//#include <iostream>
+//#include <string>
+//#include<cmath>
+//using namespace std;
+//
+//int main() {
+//    string name;
+//    int years, compound;
+//    double depositAmount = 0;
+//    double rate;
+//    double result;
+//    cout << "enter name: ";
+//    cin >> name;
+//    cout << "enter years: ";
+//    cin >> years;
+//    cout << "enter deposit amount: ";
+//    cin >> depositAmount;
+//    cout << "enter rate: ";
+//    cin >> rate;
+//    cout << "enter compound: ";
+//    cin >> compound;
+//    result = (depositAmount * pow(1 + (rate / (100 * compound)), (years * compound)));
+//    result = result - depositAmount;
+//    cout << "Hello, " << name  << "" << endl;
+//    cout << "The simple inetrest of " << depositAmount << " at a rate of " << rate << "%" << " for " << years << "years is " << result << endl;
+//}
+
