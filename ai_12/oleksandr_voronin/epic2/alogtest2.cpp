fa1e93bf529ae1615bb2a2b902c5058baed467ae
mantix <oleksandr.voronin.shi.2023@lpnu.ua>
2023-11-06 22:54:41 +0200

    Epic2

--- ai_12/oleksandr_voronin/epic2/alogtest2.cpp
+++ ai_12/oleksandr_voronin/epic2/alogtest2.cpp
@@ -1,80 +1,42 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-int main(){
-
-    int n[5]={9,7,4,2,5};
-    int fatigue;
-    int i;
-    int m;
-// виводимо елементи масиву
-    for (int i = 0; i < 5; i++)
+void BubleSort(int road[], int elements)
+{
+    for (int i = 0; i < elements-1; i++)
     {
-        cout << n[i] << " ";
-    }
-    cout << endl;
-
-    
-    int min = n[0];
-    int max = n[0];
-    //Знаходимо мінімальний елемент масиву з 2 елементу
-    for (int i = 1; i < 5; i++)
-    {
-        if (min > n[i])
+    for (int j = i + 1; j < elements; j++)
         {
-            min = n[i];
-        }
-        
+        if(road[j]<road[i])
+            {
+            swap (road[j],road[i]);
+            }
+        } 
     }
-    cout <<"min "<< min << endl;
-    //Знаходимо максимальне значення елемент масиву з 2 елементу
-    for (int i = 1; i < 5;i++)
-    {
-        if (max < n[i])
-        {
-            max = n[i];
-        }
-    }
-    cout <<"max "<< max << endl;
-   //Знаходжу втому
-   fatigue = max-min;
-   cout << "fatigue " << fatigue << endl;
-   //Видалити 1 елемент з масиву
-  for (int i = 3; i < 5; i=i+1)
-  {
-    n[i]=n[i + 1];
-  }
-  //зсуваю маcив на 1 елемент,тобто n[3] міняю на n[4] 
-  for (int i = 0; i < 4; i=i+1)
-  {
-    cout << "  "<<n[i];
-  }
-  cout << endl;
-  //шукаєм max i min в меншому масиві
+}
+int main()
+{
 
-    int mmin = n[0];
-    int mmax = n[0];
+int elements;
+cout <<"enter size of array \n";
+cin >> elements;
+int road[elements];
+cout << "Enter elements of array \n";
+for (int i = 0; i < elements; i++)
+{
+    cin >> road[i];
+}
+BubleSort(road,elements);
 
-  for (int i=1;i<4;i++)
-  {
-    if (mmin > n[i])
+int NewElements=elements-1;
+int NewRoad[NewElements];
+for(int i=0;i<elements; i++) 
     {
-        mmin = n[i];
+        NewRoad[i - 1] = road[i];
     }
-  }
-   cout << "mmin " << mmin <<endl;
-   for (int i=1;i<4;i++)
-   {
-    if (mmax < n[i])
-    {
-        mmax = n[i];
-    }
-   }
-    cout << "mmax " << mmax <<endl;
-    m = mmax - mmin;
-    cout << "m: "<< m << endl;
-    
-   
-    return 0;
+int min=NewRoad[0];
+int max=NewRoad[NewElements-1];
+int m = max - min;
+cout <<"m "<< m << endl;
+return 0;
 }
