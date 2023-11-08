918d10ab77fe7710d1a332f8a757f2a5d4672a48
Motruk Oleg <oleh.motruk.shi.2023@lpnu.ua>
2023-11-08 21:07:44 +0200

    Epic2_algo_task_1_updated

--- ai_14/oleh_motruk/Epic2/algotester_lab_1_task_Motruk_Oleg.cpp.cpp
+++ ai_14/oleh_motruk/Epic2/algotester_lab_1_task_Motruk_Oleg.cpp.cpp
@@ -5,32 +5,29 @@ using namespace std;
 
 int main() {
     const int n = 5;
-    int numbers[n];
+    long long numbers[n];
     string check;
 
     cout << "Enter a side of cube:" << endl;
     for (int i = 0; i < n; i++) {
         cin >> numbers[i];
         if (numbers[i] <= 0) {
-            cout << "Side of cube can not be 0" << endl;
+            cout << "ERROR" << endl;
             exit(0);
         }
     }
 
     for (int i = 0; i < n; i++) {
         if (numbers[i] > numbers[i -1] ){
-            check = "Lose";
+            check = "LOSS";
         }
         else if (numbers[i] < numbers[i - 1]){
-            check = "Win";
+            check = "WIN";
         }
     }
 
-    if (check == "Lose") {
-        cout << "Lose" << endl;
-    }
-    else if (check == "Win"){
-        cout << "win" << endl;
-    }
+    
+    cout << check << endl;
+
     return 0;
 }
