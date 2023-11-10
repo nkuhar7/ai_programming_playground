f72215ecc84ccc6b58689d2975f0f261c0691ca3
kuzoivan <ivan.kuzo.shi.2023@lpnu.ua>
2023-11-10 10:52:33 +0200

    report epic1 and epic 2

--- ai_13/ivan_kuzo/epic 2/Algo1.1.cpp
+++ ai_13/ivan_kuzo/epic 2/Algo1.1.cpp
@@ -1,34 +1,30 @@
 #include<iostream>
-#include<cmath>
 using namespace std;
 int main()
 {
-    long long hi;
-    long long mi;
-    long long H;
-    long long M;
-    cout<<"Enter hp and mana: ";
+    string condition;
+    condition="YES";
+    long long int H,M,hi,mi;
     cin>>H>>M;
     if(H<1)
         H=1;
-    if(H<1)
+    if(M<1)
         M=1;
-    string condition="YES";
     for (int i=0;i<3;i++)
     {
-        cout<<"Enter price(hp and mana): "<<endl;
         cin>>hi>>mi;
+        if(hi<0)
+            hi=0;
+        if(mi<0)
+            mi=0;
         if(hi>0 && mi>0){
-            condition ="NO";
+            condition="NO";
         }
         H-=hi;
         M-=mi;
         if (H<=0 || M<=0){
             condition="NO";
         }
-        cout<<"H: "<<H<<endl;
-        cout<<"M: "<<M<<endl;
-
     }
     cout<<condition;
 
