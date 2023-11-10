01a85d131afaf2a7fbe3facf0ace6f5f8e20fd00
vorobmaks <146077660+vorobmaks@users.noreply.github.com>
2023-11-10 15:40:01 +0200

    Update algotester_lab_1_task_max_vorobets.cpp

--- ai_12/maksym_vorobets/algotester_lab_1_task_max_vorobets.cpp
+++ ai_12/maksym_vorobets/algotester_lab_1_task_max_vorobets.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    unsigned h1, h2, h3, h4;
-    unsigned d1, d2, d3, d4;
-    scanf("%d %d %d %d", &h1, &h2, &h3, &h4);
-    scanf("%d %d %d %d", &d1, &d2, &d3, &d4);
+    unsigned long long h1, h2, h3, h4;
+    unsigned long long d1, d2, d3, d4;
+    scanf("%lld %lld %lld %lld", &h1, &h2, &h3, &h4);
+    scanf("%lld %lld %lld %lld", &d1, &d2, &d3, &d4);
         int min_h=h1;
         if(h2<min_h){min_h=h2;}
         if(h3<min_h){min_h=h3;}
@@ -14,7 +14,8 @@ int main(){
         if(h2>max_h){max_h=h2;}
         if(h3>max_h){max_h=h3;}
         if(h4>max_h){max_h=h4;}
-    if(2*min_h>=max_h&&min_h!=0){
+
+    if(2*min_h>max_h&&min_h!=0&&2*h1-2*d1>max_h&&2*h2-2*d2>max_h&&2*h3-2*d3>max_h&&2*h4-2*d4>max_h){
         printf("YES\n");
     }else if(d1>h1||d2>h2||d3>h3||d4>h4){
         printf("ERROR\n");
@@ -24,6 +25,7 @@ int main(){
     
     return 0;
 }
+
 
 
 
