41688e98bf35aef531c97787a10f70d7194918ab
olehsmshi <oleh.smerekanych.shi.2023@lpnu.ua>
2023-11-10 20:40:57 +0200

    Added report 1

---
+++ ai_14/oleh_smerekanych/Epic1/selfpractice_algo_0001.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a, b, result;
+
+cout << "Enter A and B: ";
+cin >> a >> b;
+
+result = a + b;
+cout << "The result of a + b is: " << result;
+    return 0;
+}
