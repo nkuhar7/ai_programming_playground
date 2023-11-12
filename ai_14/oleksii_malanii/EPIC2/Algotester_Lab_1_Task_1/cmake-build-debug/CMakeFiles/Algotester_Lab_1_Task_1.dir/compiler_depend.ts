1dd0d887db6a139e9d561df8b3b316ba5781ed4e
OleksiiMalanii <146111996+OleksiiMalanii@users.noreply.github.com>
2023-11-12 23:33:45 +0200

    Delete ai_14/oleksii_malanii/EPIC2 directory

--- ai_14/oleksii_malanii/EPIC2/Algotester_Lab_1_Task_1/cmake-build-debug/CMakeFiles/Algotester_Lab_1_Task_1.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for Algotester_Lab_1_Task_1.
--- ai_14/oleksii_malanii/EPIC2/Algotester_Lab_1_Task_1/cmake-build-debug/CMakeFiles/Algotester_Lab_1_Task_1.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Algotester_Lab_1_Task_1.
-# This may be replaced when dependencies are built.
