a2ff19867f1b0abfa0e607a5e507bd03145b2e7f
Motruk Oleg <oleh.motruk.shi.2023@lpnu.ua>
2023-11-14 00:14:53 +0200

    Algo1_task_passed_upd

--- ai_14/oleh_motruk/Epic2/algotester_lab_1_task_Motruk_Oleg.cpp.cpp
+++ ai_14/oleh_motruk/Epic2/algotester_lab_1_task_Motruk_Oleg.cpp.cpp
@@ -12,18 +12,14 @@ int main() {
         cin >> numbers[i];
         if (numbers[i] <= 0) {
             cout << "ERROR" << endl;
+        }
+            exit(0);
+        if (numbers[i] > numbers[i -1] ){
+            check = "LOSS";
             exit(0);
         }
     }
 
-    for (int i = 0; i < n; i++) {
-        if (numbers[i] > numbers[i -1] ){
-            check = "LOSS";
-        }
-        else if (numbers[i] < numbers[i - 1]){
-            check = "WIN";
-        }
-    }
 
     
     cout << check << endl;
