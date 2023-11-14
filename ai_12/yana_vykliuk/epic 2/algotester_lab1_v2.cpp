ce03ab415e18291fe03ed27da2d8dc7b8752fce2
Yana Vyklyuk <yana.vykliuk.shi.2023@lpnu.ua>
2023-11-14 14:27:59 +0200

    Changed review

--- ai_12/yana_vykliuk/epic 2/algotester_lab1_v2.cpp
+++ ai_12/yana_vykliuk/epic 2/algotester_lab1_v2.cpp
@@ -2,34 +2,58 @@
 
 using namespace std;
 
-int OriginalHeight1, OriginalHeight2, OriginalHeight3, OriginalHeight4;
-int Cut1, Cut2, Cut3, Cut4; 
+long long Height1, Height2, Height3, Height4;
+long long Cut1, Cut2, Cut3, Cut4; 
 
 int main ()
 {
-    cin >> OriginalHeight1 >> OriginalHeight2 >> OriginalHeight3 >> OriginalHeight4;
+    cin >> Height1 >> Height2 >> Height3 >> Height4;
     cin >> Cut1 >> Cut2 >> Cut3 >> Cut4;
 
-    if (Cut1>OriginalHeight1 || Cut2>OriginalHeight2 || Cut3>OriginalHeight3 || Cut4>OriginalHeight4)
+    if (Cut1>Height1 || Cut2>Height2 || Cut3>Height3 || Cut4>Height4)
     {
         cout << "ERROR" << endl;
+        return 0;
     }
-    else
-    {
-        int FinalHeight1 = OriginalHeight1 - Cut1;
-        int FinalHeight2 = OriginalHeight2 - Cut2;
-        int FinalHeight3 = OriginalHeight3 - Cut3;
-        int FinalHeight4 = OriginalHeight4 - Cut4;
 
-        if ((2*FinalHeight1>OriginalHeight2 || 2*FinalHeight1>OriginalHeight3 || 2*FinalHeight1>OriginalHeight4) && (2*FinalHeight2>OriginalHeight3 || 2*FinalHeight2>OriginalHeight4) && 2*FinalHeight3>OriginalHeight4)
+    Height1 -= Cut1;
+    long long max1= max(max(Height2,Height3),Height4);
+    if(2*Height1>max1)
+    {
+        Height2 -= Cut2;
+        long long max2= max(Height3,Height3);
+        long long min2= min(Height1, Height2);
+        if(2*min2>max2)
         {
-            cout << "YES" << endl;   
+            Height3 -= Cut3;
+            long long min3= min(min(Height1,Height2),Height3);
+            if(2*min3>Height4)
+            {
+                Height4 -= Cut4;
+                if(Height1==Height2 && Height1==Height3 && Height1==Height4)
+                {
+                    cout << "YES" << endl;
+                }
+                else
+                {
+                    cout << "NO" << endl;
+                }
+            }
+            else
+            {
+                cout << "NO" << endl;
+            }
         }
         else
         {
             cout << "NO" << endl;
         }
     }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
 
     return 0;
 }
