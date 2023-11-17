4d34ace2bccf4d4d421897ebf8deb5c2ce9d7cf7
AndrewVynnytskyi <nikoljavin@gmail.com>
2023-11-17 14:55:23 +0200

    Add new files

---
+++ ai_11/andrii_vynnytskyi/Epic1/self_practice_1_task_andrew_vynnytskyi.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int number;
+    cin >> number;
+    int factorial = 1;
+    for(int i = 1; i <= number; i++ )
+    {
+       factorial*=i;
+        
+    }
+    cout << factorial;
+}
