ca76db2bb75bb5819b7449b5bc692d5be2babc8d
Dak <danylo.kvit.shi.2023@lpnu.ua>
2023-11-18 14:53:53 +0100

    2 programs and draw.io

---
+++ ai_11/danylo_kvit/Epic-1/complex-pers.c
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+int main(){
+    char s[] = "Hello World";
+    printf("%s", s);
+}
+
+
+Dear All,
+
+        Here is the link to the recording of our group call, for those who missed or would like to re-watch.
+
+https://us02web.zoom.us/rec/share/GzeZXPngQ27DfWehTo0Zm1bhD14lxrTblNP6y6-7FOirDHs8FRSAt_qknxzLnYA8.YEYvPvO3gdk3Ngew
+Passcode: d$ju30%8
