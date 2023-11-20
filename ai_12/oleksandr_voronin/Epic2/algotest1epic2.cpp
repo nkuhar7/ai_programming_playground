dc57119022013cba91b8d79f74bbe9968bcc9bee
mantix <oleksandr.voronin.shi.2023@lpnu.ua>
2023-11-20 19:45:30 +0200

    Update_epic_2_Voronin_Oleksandr

--- ai_12/oleksandr_voronin/Epic2/algotest1epic2.cpp
+++ ai_12/oleksandr_voronin/Epic2/algotest1epic2.cpp
@@ -2,39 +2,56 @@
 using namespace std;
 
 int main() {
-  long long int h1, h2, h3, h4;
-  long long int d1, d2, d3, d4;
+  long long h1, h2, h3, h4;
+  long long d1, d2, d3, d4;
+  
     
-    //cout << "Type size of chair legs: ";
     cin >> h1 >> h2 >> h3 >> h4;
 
-  
-    cin >> d1;
-    cin >> d2;
-    cin >> d3;
-    cin >> d4;
-
-    long long int a1= h1-d1;
-    long long int a2= h2-d2;
-    long long int a3= h3-d3;
-    long long int a4= h4-d4;
+    cin >> d1 >> d2 >> d3 >> d4;
     
-    long long int minLeg = min(min(min(h1, h2), h3), h4);
-    long long int maxLeg = max(max(max(h1, h2), h3), h4);
-
-    
-
-    // Перевірка на ERROR
-    if (d1 > h1 || d2 > h2 || d3 > h3 || d4 > h4) {
-        cout << "ERROR" << endl;
+     if (d1 > h1 ||  d2 > h2 || d3 > h3 || d4 > h4) 
+    {
+        cout << " ERROR" << endl;
+        return 0;
     }
-    // Перевірка чи ніжки не перевернуться
-    else if (maxLeg >= 2 * minLeg) 
+
+    h1 -= d1;
+    
+    long long minLeg = min(min(min(h1, h2), h3), h4);
+    long long maxLeg1 = max(max(max(h1, h2), h3), h4);
+    
+
+    if (maxLeg1 >= 2 * minLeg) 
     {
         cout << "NO" << endl;
+        return 0;
     }
-    // Перевірка чи ніжки рівні чи ні
-    else if(a1 == a2 && a2 == a3 && a3 == a4)
+    h2 -= d2;
+    long long maxLeg2 = max(max(max(h1, h2), h3), h4);
+    if (maxLeg2 >= 2 * minLeg) 
+    {
+        cout << "NO" << endl;
+        return 0;
+    }
+    h3 -= d3;
+    long long maxLeg3 = max(max(max(h1, h2), h3), h4);
+    if (maxLeg3 >= 2 * minLeg) 
+    {
+        cout << "NO" << endl;
+        return 0;
+    }
+    h4 -= d4;
+    long long maxLeg4 = max(max(max(h1, h2), h3), h4);
+    if (maxLeg4 >= 2 * minLeg) 
+    {
+        cout << "NO" << endl;
+        return 0;
+    }
+
+
+
+    if(h1 == h2 && h2 == h3 && h3 == h4)
     {
         cout << "YES" << endl;
     }
--- ai_12/oleksandr_voronin/Epic2/pogoda.cpp
+++ ai_12/oleksandr_voronin/Epic2/pogoda.cpp
@@ -5,27 +5,54 @@ using namespace std;
 int main(){
 
 string weather;
+int shoes;
 cout <<"Whrite your weathe conditions \n";
 cin >> weather;
 if (weather=="snowly"||"windly"){
 cout << "you need cuphead \n";
+shoes = 1;
 }
 else {cout << "you dont need cuphead \n";}
  if (weather=="sunny")
+ 
 {
-    cout << "you can wear T-shirt,shorts and crocs \n";
-} else if (weather=="rainy")
+    cout << "you can wear T-shirt,shorts \n";
+    shoes =2;
+}
+ else if (weather=="rainy")
 {
-cout << "wear boots and umbrella \n";
+cout << "take an umbrella \n";
+shoes = 3;
 }
  else if (weather=="cloudly"){
     cout << "wear pants and hoodie \n";
-
+   shoes = 4;
  }
  else if (weather=="snowly"){
     cout << "wear boots,pants and jacket \n";
+    shoes = 1;
  }
  else if (weather=="windly"){
     cout << "Look at the window bro,you dont need that \n";
+    shoes = 6;
+ }
+
+ switch (shoes)
+ {
+ case 1:
+   cout << "Term shoes " << endl;
+   break;
+ case 2:
+   cout <<"Crocs " <<endl;
+   break;
+ case 3:
+   cout <<"Boots " <<endl;
+   break;
+ case 4:
+   cout <<"Slippers " <<endl;
+   break;
+  case 6:
+   cout <<"Sneakers " <<endl;
+   break;
  }
 }
