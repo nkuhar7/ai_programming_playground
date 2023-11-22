3faa2c73f1e57cf2de949916c0925f268fcb9196
Sofia Pryslipska <sofiia.pryslipska.shi.2023@lpnu.ua>
2023-11-22 22:34:57 +0200

    corrected the template

--- ai_13/sofiia_pryslipska/EPIC 2/vns_lab_1_task_1_sofia_pryslipska.cpp
+++ ai_13/sofiia_pryslipska/EPIC 2/vns_lab_1_task_1_sofia_pryslipska.cpp
@@ -11,8 +11,8 @@ double action2=pow(a,3);
 double action3=3*a*pow(b,2);
 double action4=pow(b,3);
 double action5=3*pow(a,2)*b;
+double result=(action1-action2)/(action3+action4+action5);
 
-double result=((pow(a+b, 3))-(pow(a,3)))/((3*a*pow(b,2))+(pow(b,3))+(3*pow(a,2)*b));
 cout <<result<< endl;
 
 
@@ -23,8 +23,11 @@ float action2_f=pow(a_f,3);
 float action3_f=3*a_f*pow(b_f,2);
 float action4_f=pow(b_f,3);
 float action5_f=3*pow(a_f,2)*b_f;
+float result_f=(action1_f-action2_f)/(action3_f+action4_f+action5_f);
 
-float result_f=((pow(a_f+b_f, 3))-(pow(a_f,3)))/((3*a_f*pow(b_f,2))+(pow(b_f,3))+(3*pow(a_f,2)*b_f));
 cout <<result_f<< endl;
 return 0;
 }
+
+//float result_f=((pow(a_f+b_f, 3))-(pow(a_f,3)))/((3*a_f*pow(b_f,2))+(pow(b_f,3))+(3*pow(a_f,2)*b_f));
+
