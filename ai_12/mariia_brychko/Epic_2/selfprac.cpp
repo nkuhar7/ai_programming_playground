b837df02d02874d7b66425562c25ad5c78aaff80
Maria Brychko <mariia.brychko.shi.2023@lpnu.ua>
2023-11-23 13:41:16 +0200

    delete file

--- ai_12/mariia_brychko/Epic_2/selfprac.cpp
+++ ai_12/mariia_brychko/Epic_2/selfprac.cpp
@@ -4,56 +4,47 @@
 using namespace std;
 int main() {
     int n;
-    cout << "Enter the number of rounds: ";
-    cin >> n;
-    while  (n < 1 || n > 100000LL) {
-        cout << "The number of rounds must be from 1 to 100000.\n";
-        cout << "Enter the number of rounds: ";
-        cin >> n;           
-}
+    cin >> n;           
+
     for (int i = 0; i < n; i++)
     {
         if (n % 2 == 0) {
-        cout << "The number of rounds must be odd.\n";
-        cout << "Enter the number of rounds: ";
+        cout << "Error\n";
         cin >> n;
     }
     }
+    
 vector<int> a(n);      
 vector<int> b(n);
+
  for (int i = 0; i < n; i++) {
-    cout << "Enter your " << i+1 << " card number: ";
         cin >> a[i];
-         if (a[i] > 2*n)
-    {
-        cout << "The number cannot be more than the number of cards\n";
-        cout << "Enter your " << i+1 << " card number: ";
-        cin >> a[i];
+         if (a[i] > 2*n){
+        cout << "Error\n";
+        cin >> a[i]; }
     }
-    }
+
     for (int i = 0; i < n; i++) {
-         cout << "Enter the opponent\'s " << i+1 << " card number: ";
         cin >> b[i];
-        if (b[i] > 2*n)
-    {
-        cout << "The number cannot be more than the number of cards\n";
-        cout << "Enter the opponent\'s " << i+1 << " card number: ";
-        cin >> b[i];
-        }
+        if (b[i] > 2*n) {
+        cout << "Error\n";
+        cin >> b[i]; }
     }
+
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
+
     for (int i = 0; i < n; i++) {
-        if (a[i] == b[i])
-    {
-        cout << "Your and your opponent's numbers cannot be the same\n";
-        cout << "Enter the opponent\'s " << i+1 << " card number: ";
-        cin >> b[i];}
-    }
+         if (a[i] == a[i-1] || b[i] == b[i-1] || a[i] == b[i-1] || b[i] == a[i-1] || a[i] == b[i]) {    //тут помилка
+        cout << "Error\n"; 
+        return 0;
+        } 
+        }
+
 int yourWins = 0;
 int opponentWins = 0;
     for (int i = 0; i < n; i++) {
-        if (a[i] > b[i]) {
+        if (a[i] > b[i]) {    //тут помилка
             yourWins++;
         } else if (a[i] < b[i]) {
             opponentWins++;
