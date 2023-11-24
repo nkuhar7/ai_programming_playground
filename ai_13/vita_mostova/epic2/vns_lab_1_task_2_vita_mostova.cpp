43b85554d8279bab05e88addc6189ee4870ff959
unknown <Vita.Mostova.ShI.2023@lpnu.ua>
2023-11-24 09:50:45 +0200

    Epic №2 - Vita Mostova: edit some details

--- ai_13/vita_mostova/epic2/vns_lab_1_task_2_vita_mostova.cpp
+++ ai_13/vita_mostova/epic2/vns_lab_1_task_2_vita_mostova.cpp
@@ -8,9 +8,9 @@ int main(){
     cout<<"m = ";
     cin>>m;
     float a = m+--n;
-    float b = m++<++n;
-    float c = n--<--m;
-    cout<<"Results are : "<<a<<endl<<b<<endl<<c<<endl;
+    bool b = m++<++n;
+    bool c = n--<--m;
+    cout<<"Results are :\n"<<a<<endl<<b<<endl<<c<<endl;
     
     return 0;
 
