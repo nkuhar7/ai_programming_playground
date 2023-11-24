7d7531486f86c67ba5ca4b56c61530e4e11b96a1
DariaFesenko <daria.fesenko.shi.2023@lpnu.ua>
2023-11-25 00:46:58 +0200

    Epic 2 Daria Fesenko

---
+++ ai_13/daria_fesenko/epic_2_pactice_and_labs_daria_fesenko/vns_lab_1_task_2.2_daria_fesenko.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+
+float n, m;
+cout << "Enter n=";
+cin >> n;
+
+cout << "Enter m=";
+cin >> m;
+
+float result = m++<n;
+cout << "result is " << result << endl;
+
+return 0;
+
+}
+
