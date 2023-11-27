6a5b8d96e7c7338b0388bbb0e24f23ae2c11052b
Zimnov1 <oleksandr.zimnov.shi.2023@lpnu.ua>
2023-11-27 20:49:57 +0200

    Add change to VNS Lab 3

--- ai_14/oleksandr_zimnov/Epic_3/vns_lab_3_task_zimnov_oleksandr/vns_lab_3_task_zimnov_oleksandr.cpp
+++ ai_14/oleksandr_zimnov/Epic_3/vns_lab_3_task_zimnov_oleksandr/vns_lab_3_task_zimnov_oleksandr.cpp
@@ -7,19 +7,16 @@ using namespace std;
 int main()
 {
     int n=10;
-    double k=(0.8-0.1)/n, sum_a=0, x_a=0;
+    double k=(0.8-0.1)/n, sum_x=0, x_a=0, sum_y=0;
     for(int i=1; i<=n; i++){
         x_a+=k;
-        sum_a+=pow(-1,i+1)*((pow(x_a,2*i))/(2*i*(2*i-1)));
+        sum_x+=pow(-1,i+1)*((pow(x_a,2*i))/(2*i*(2*i-1)));
+        sum_y+=x_a*atan(x_a)-log(sqrt(1+pow(x_a,2)));
     }
-    cout<<"a) Sum of number for n=10: "<<sum_a<<endl;
-    double x_b=0, sum_b=0;
-    int temp=0;
-    do{
-        x_b+=k;
-        temp++;
-        sum_b+=pow(-1,temp+1)*((pow(x_b,2*temp))/(2*temp*(2*temp-1)));
-    }
-    while(sum_b>=0.0001);
-    cout<<"b) Sum of number epsilon=0.0001: "<<sum_b;
+    cout<<"a) Sum of number for n=10"<<endl;
+    cout<<"x="<<sum_x<<endl;
+    cout<<"y="<<sum_y<<endl;
+    cout<<"b) Sum of number epsilon=0.0001"<<endl;
+    cout<<"x="<<round(sum_x*10000)/10000<<endl;
+    cout<<"y="<<round(sum_y*10000)/10000;
 }
