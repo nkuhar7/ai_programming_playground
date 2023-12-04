a32db032ca3bde7b6df7d515aa6eea35e873b6b6
Daniel Shliakhetko <danielshliakhetko@gmail.com>
2023-12-04 12:06:36 +0200

    Fixed merge conflicts

--- ai_11/danylo_shliakhetko/epic_2/algotester_lab_1_task_danylo_shliakhetko.cpp
+++ ai_11/danylo_shliakhetko/epic_2/algotester_lab_1_task_danylo_shliakhetko.cpp
@@ -5,57 +5,31 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
     vector<long long> tableLegs = {};
 
     for (int i = 0; i < 4; i++)
     {
         long long tableLeg;
-=======
-    vector<int> tableLegs = {};
-    for (int i = 0; i < 4; i++)
-    {
-        int tableLeg;
->>>>>>> 45dbc78efeca21bbd191edf1130e7b647bc3ae26
         cin >> tableLeg;
         tableLegs.push_back(tableLeg);
     }
 
-<<<<<<< HEAD
     vector<long long> tableCuts = {};
 
     for (int i = 0; i < 4; i++)
     {
         long long tableCut;
-=======
-    vector<int> tableCuts = {};
-
-    for (int i = 0; i < 4; i++)
-    {
-        int tableCut;
->>>>>>> 45dbc78efeca21bbd191edf1130e7b647bc3ae26
         cin >> tableCut;
         tableCuts.push_back(tableCut);
     }
 
-<<<<<<< HEAD
     for (int i = 0; i < 4; i++)
     {
         if (tableCuts[i] > tableLegs[i])
-=======
-    int maxLeg = tableLegs[0];
-    int minLeg = tableLegs[0];
-
-    for (int i = 0; i < tableLegs.size(); i++)
-    {
-        int currentLeg = tableLegs[i] - tableCuts[i];
-        if (currentLeg <= 0)
->>>>>>> 45dbc78efeca21bbd191edf1130e7b647bc3ae26
         {
             cout << "ERROR";
             return 0;
         }
-<<<<<<< HEAD
     }
 
     long long minLeg = 0;
@@ -99,26 +73,3 @@ int main()
         cout << "NO";
     }
 }
-=======
-        if (maxLeg < currentLeg)
-        {
-            maxLeg = currentLeg;
-        }
-        if (minLeg > currentLeg)
-        {
-            minLeg = currentLeg;
-        }
-    }
-
-    if (maxLeg > 2 * minLeg)
-    {
-        cout << "NO";
-    }
-    else
-    {
-        cout << "YES";
-    }
-
-    return 0;
-}
->>>>>>> 45dbc78efeca21bbd191edf1130e7b647bc3ae26
