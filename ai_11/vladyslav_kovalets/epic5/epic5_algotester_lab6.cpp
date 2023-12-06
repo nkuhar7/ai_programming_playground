1e182c5588d94bee3a07c96061aa74dd65bbff57
kovalets-vlad <vladyslav.kovalets.shi.2023@gmail.com>
2023-12-06 20:56:42 +0200

    update algotester6 task1

--- ai_11/vladyslav_kovalets/epic5/epic5_algotester_lab6.cpp
+++ ai_11/vladyslav_kovalets/epic5/epic5_algotester_lab6.cpp
@@ -1,107 +1,90 @@
 #include <iostream>
 #include <string>
 #include <cctype>
-#include <set>
+#include <map>
+#include <vector>
+#include <algorithm>
 
 int main() 
 {
-    unsigned long long N, K;
-    bool off = false;  
+    unsigned int N, K;
 
     // Зчитуємо кількість слів (N) та мінімальну кількість повторень (K)
     std::cin >> N;
-    unsigned long long count[100000] = {};  
-
+    std::vector<unsigned long long> count(N, 0);
     std::cin >> K;
 
-    // Створюємо динамічний масив для зберігання слів
-    std::string* words = new std::string[N];
+    // Створюємо вектор для зберігання слів
+    std::vector<std::string> words(N);
 
-    // Зчитуємо слова у масив
-    for (int i = 0; i < N; i++) 
+    // Виводимо унікальні букви для слів, які мають принаймні K повторень
+    std::map<char, bool> uniqueChars;
+
+    // Зчитуємо слова у вектор
+    for (int i = 0; i < N; ++i) 
     {
         std::cin >> words[i];
     }
 
     // Обчислюємо кількість повторень для кожного слова
-    for (int i = 0; i < N; i++) 
+    for (int i = 0; i < N; ++i) 
     {
-        if (words[i] == "") 
+        if (words[i].empty()) 
         {
             continue;
         }
 
         count[i] = 1;
 
-        for (int j = i + 1; j < N; j++) 
+        for (int j = i + 1; j < N; ++j) 
         {
-            if (!words[j].empty())
+            if (words[j].empty()) 
             {
-                // Convert each character in words[j] to uppercase and compare
-                bool areEqual = true;
-                for (int k = 0; k < 11; k++)
-                {
-                    if (std::toupper(words[j][k]) != std::toupper(words[i][k]))
-                    {
-                        areEqual = false;
-                        break;
-                    }
-                }
+                continue;
+            }
 
-                if (areEqual)
-                {
-                    count[i]++;
-                    words[j] = "";
-                }
+            if (std::equal(words[i].begin(), words[i].end(), words[j].begin(),
+                           [](char a, char b) { return std::toupper(a) == std::toupper(b); })) 
+            {
+                count[i]++;
+                words[j] = "";
+            }
+        }
+
+        if (count[i] >= K) 
+        {
+            for (char c : words[i]) 
+            {
+                uniqueChars[std::tolower(c)] = false; // Додаємо символ до map
             }
         }
     }
 
-    int number = 0;
-    for (int i = 0; i < N; i++) 
+    // Виводимо результат
+    if (uniqueChars.size() == 0) 
     {
-        if (count[i] >= K) 
-        {
-            std::set<char, std::greater<char>> uniqueChars;
-            for (char c : words[i]) 
-            {
-                uniqueChars.insert(std::tolower(c));
-            }
-            // Виводимо кількість унікальних букв
-            number += uniqueChars.size();
-        }     
-    }
-
-    if (number != 0)
-        std::cout << number << std::endl;
-    else
         std::cout << "Empty!" << std::endl;
-
-    // Виводимо унікальні букви для слів, які мають принаймні K повторень
-    for (int i = 0; i < N; i++) 
-    {
-        if (count[i] >= K) 
-        {
-            std::set<char, std::greater<char>> uniqueChars;
-
-            // Заповнюємо множину унікальними буквами слова
-            for (char c : words[i]) 
-            {
-                uniqueChars.insert(std::tolower(c));
-            }
-
-            // Виводимо унікальні букви
-            for (char c : uniqueChars) 
-            {
-                std::cout << c << " ";
-            }
-
-            off = true;   
-        }
+        return 0;
     }
 
-    // Звільняємо виділену пам'ять для масиву слів
-    delete[] words;
+    // Виводимо кількість символів
+    std::cout << uniqueChars.size() << std::endl;
+
+    // Створюємо вектор для зберігання унікальних букв у порядку
+    std::vector<char> sortedChars;
+    for (const auto& entry : uniqueChars) 
+    {
+        sortedChars.push_back(entry.first);
+    }
+
+    // Сортуємо вектор у зворотньому порядку
+    std::sort(sortedChars.rbegin(), sortedChars.rend());
+
+    // Виводимо унікальні букви в зворотньому порядку
+    for (char c : sortedChars) 
+    {
+        std::cout << c << " ";
+    }
 
     return 0;
 }
