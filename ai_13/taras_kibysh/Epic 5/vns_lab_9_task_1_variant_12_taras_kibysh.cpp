744e80ce58bd0b5c966bdb5bb2bc9da6d00615a7
Kibysh Taras <taras.kibysh,shi.2023@lpnu.ua>
2023-12-08 20:44:54 +0200

    some change

--- ai_13/taras_kibysh/Epic 5/vns_lab_9_task_1_variant_12_taras_kibysh.cpp
+++ ai_13/taras_kibysh/Epic 5/vns_lab_9_task_1_variant_12_taras_kibysh.cpp
@@ -14,8 +14,27 @@ int countVowels(const std::string& str) {
     }
     return vowelCount;
 }
+void createFirstFile(std::string str){
+    std::ofstream outputFile("F1.txt", std::ios::trunc);
+     if (outputFile.is_open()) {
+        outputFile << "Bright stars hung in the dark sky, resembling diamonds on a black velvet blanket.\n";
+        outputFile << "The aroma of freshly brewed coffee pierces the morning mist, filling the kitchen with warmth and comfort.\n";
+        outputFile << "The laughter of children echoes in the backyard, spreading joy and merriment.\n";
+        outputFile << "The babbling brook softly whispers among the rocks, creating a natural symphony of forest life.\n";
+        outputFile << "The vibrant colors of spring flowers blossom in the park, awakening nature from its winter slumber.\n";
+        outputFile << "The sun sets on the horizon, painting the sky in shades of pink and orange.\n";
+        outputFile << "A gentle breeze embraces the face, carrying the scent of the sea and distant adventures.\n";
+        outputFile << "Ancient streets in the city center glisten with reflections from evening lanterns.\n";
+        outputFile << "The soft purple light of street café lanterns creates an atmosphere of warmth and romance.\n";
+        outputFile << "Fresh fruits sit on the table in the room, enticing appetite and promising a flavorful day.\n";
+        // Закрити файл
+        outputFile.close();
+}
+}
 
 int main() {
+    std::string name = "F1.txt";
+    createFirstFile(name);
     std::ifstream inputFile("F1.txt"); // Відкриваємо вхідний файл для читання
     std::ofstream outputFile("F2.txt",std::ios::out | std::ios::trunc); // Відкриваємо вихідний файл для запису
 
