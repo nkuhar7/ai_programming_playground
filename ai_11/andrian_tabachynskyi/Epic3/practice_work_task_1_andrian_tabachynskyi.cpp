c0665cb26b603f9b08d5efda31daf28744cf712d
@Andrian03 <andrian.tabach@gmail.com>
2023-12-10 13:13:21 +0200

    Add files

--- ai_11/andrian_tabachynskyi/Epic3/practice_work_task_1_andrian_tabachynskyi.cpp
+++ ai_11/andrian_tabachynskyi/Epic3/practice_work_task_1_andrian_tabachynskyi.cpp
@@ -5,51 +5,38 @@
 using namespace std;
 
 int main() {
-    vector<string> books = { "Harry Potter", "Kings of Narnia", "The Usborne Book of Night Time", "1984","Sherlock Holmes", "Footballer"};
-    vector<bool> available = { 1, 0, 1, 1, 0, 1};
-    int yourchoice;
-    string Name, continueorno;
 
+    vector<string> books = { "Harry Potter", "Kings of Narnia", "The Usborne Book of Night Time", "1984","Sherlock Holmes", "Footballer"};
+    vector<bool> present = { 1, 0, 1, 1, 0, 1};
+    string Name, prodov;
+    int yourchoice;
+
+
+    here:
     do {
-        cout << "Menu:\n"<< "1. List all books\n"<< "2. Take a book\n"<< "3. Return a book\n"<< "4. Exit\n"<< "Choose one of the options: ";
-        cin >> yourchoice;
+        cout << "Menu:\n"<< "1. List all books\n"<< "2. Return a book\n"<< "3. Take a book\n"<< "4. Exit\n"<< "Choose one of the options: ";
+        cin >> yourchoice; 
 
         switch (yourchoice) {
         case 1:
             for (int i = 0; i < books.size(); i++) {
-                cout << books[i] << ": " << (available[i] ? "Available" : "Borrowed") << endl;
+
+                cout << books[i] << ": " << (present[i] ? "Available" : "Borrowed") << endl;
             }
             break;
 
         case 2:
             cin.ignore();
-            cout << "Enter book name to take: ";
-            getline(cin, Name);
-            for (string& book : books) {
-                if (book == Name) {
-                    int index = &book - &books[0];
-                    if (available[index]) {
-                        available[index] = 0;
-                        cout << "You take the book\n";
-                        break;
-                    }
-                    else {
-                        cout << "This book is taken\n";
-                        break;
-                    }
-                }
-            }
-            break;
 
-        case 3:
-            cin.ignore();
             cout << "Enter book name to return: ";
             getline(cin, Name);
+
+
             for (string& book : books) {
                 if (book == Name) {
-                    int index = &book - &books[0];
-                    if (!available[index]) {
-                        available[index] = 1;
+                    int i = &book - &books[0];
+                    if (!present[i]) {
+                        present[i] = 1;
                         cout << "You return the book, thank you!\n";
                         break;
                     }
@@ -61,18 +48,45 @@ int main() {
             }
             break;
 
+        case 3:
+            cin.ignore();
+
+            cout << "Enter book name to take: ";
+            getline(cin, Name);
+
+            for (string& book : books) {
+                if (book == Name) {
+
+                    int i = &book - &books[0];
+
+                    if (present[i]) {
+                        present[i] = 0;
+                        cout << "You take the book\n";
+                        break;
+                    }
+                    else {
+                        cout << "This book is taken\n";
+                        break;
+                    }
+                }
+            }
+            break;
+
         case 4:
             cout << "Goodbye!!!";
+
             return 0;
-        default:
-            cout << "Wrong choice.\n";
+
+        default: goto here;
         }
 
         cout << "Do you want to chose another operation?(Yes/No):";
-        cin >> continueorno;
-        if (continueorno == "No") {
+        cin >> prodov;
+
+        if (prodov == "No") {
             cout << "Goodbye) See you later";
             return 0;
         }
-    } while (continueorno == "Yes");
+
+    } while (prodov == "Yes");
 }
