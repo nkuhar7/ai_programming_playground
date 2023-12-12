9150801daf3c5c8d11581982c71f02131e524599
zastavnyydenys <146076442+zastavnyydenys@users.noreply.github.com>
2023-12-12 18:39:37 +0200

    Update self_practice_work_algotester_task_1_zastavnyydenys.cpp

--- ai_12/denys_zastavnyi/epic 2/self_practice_work_algotester_task_1_zastavnyydenys.cpp
+++ ai_12/denys_zastavnyi/epic 2/self_practice_work_algotester_task_1_zastavnyydenys.cpp
@@ -1,21 +1,17 @@
 #include<iostream>
-#include <iostream>
 using namespace std;
 
 int main() {
-    int n, a, totalCookies = 0;
+   long long int n, a;
+   long long int totalCookies = 0;
 
-    cout << "Enter the number of cookie packs: ";
+    
     cin >> n;
-
-    cout << "Enter the number of cookies in each pack:\n";
-    for (int i = 0; i < n; ++i) {
-        cout << "Pack " << (i + 1) << ": ";
-        cin >> a;
-        totalCookies += (a - 1);
-    }
-
-    cout << "Total cookies: " << totalCookies << endl;
-
+        
+        for (int i = 0; i < n; ++i) {
+            cin >> a;
+            totalCookies += (a - 1);
+        }
+        cout << totalCookies << endl;
     return 0;
-    }
+}
