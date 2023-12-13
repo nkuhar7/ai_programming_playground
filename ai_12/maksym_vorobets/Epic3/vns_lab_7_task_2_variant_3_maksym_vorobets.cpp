841534a8102ad268d3db8c03adf5268d73175a9b
vorobmaks <maksym.vorobets.shi.2023@lpnu.ua>
2023-12-13 15:30:26 +0200

    Epic 3 - Maksym Vorobets

--- ai_12/maksym_vorobets/Epic3/vns_lab_7_task_2_variant_3_maksym_vorobets.cpp
+++ ai_12/maksym_vorobets/Epic3/vns_lab_7_task_2_variant_3_maksym_vorobets.cpp
@@ -1,45 +1,23 @@
 #include <iostream>
+#include <complex>
+using namespace std;
 
-// Клас для представлення комплексних чисел
-class Complex {
-public:
-    double real;
-    double imag;
-
-    Complex(double r = 0.0, double i = 0.0) : real(r), imag(i) {}
-
-    // Перевантажена функція для множення комплексних чисел
-    Complex operator*(const Complex& other) const {
-        Complex result;
-        result.real = (real * other.real) - (imag * other.imag);
-        result.imag = (real * other.imag) + (imag * other.real);
-        return result;
-    }
-
-    // Перевантажена функція для виводу комплексного числа
-    friend std::ostream& operator<<(std::ostream& os, const Complex& c);
-};
-
-std::ostream& operator<<(std::ostream& os, const Complex& c) {
-    os << c.real << " + " << c.imag << "i";
-    return os;
+   double comp(double x1, double x2, double y1, double y2);
+   int comp(int a, int b);
+int main(){
+    comp(1.0, 6.0, 2.0, 4.0);
+    comp(23, 5);
+ return 0;
 }
-
-// Перевантажена функція для множення цілих чисел
-int multiply(int a, int b) {
-    return a * b;
-}
-
-int main() {
-    // Виклик функції для множення цілих чисел
-    int resultInt = multiply(5, 7);
-    std::cout << "Multiplication of integers: " << resultInt << std::endl;
-
-    // Виклик функції для множення комплексних чисел
-    Complex complex1(2.0, 3.0);
-    Complex complex2(4.0, 5.0);
-    Complex resultComplex = complex1 * complex2;
-    std::cout << "Multiplication of complex numbers: " << resultComplex << std::endl;
-
+    double comp(double x1, double x2, double y1, double y2){
+    complex< double > z1(x1, x2); //1+6i//
+    complex< double > z2(y1, y2);//2+4i//
+    cout<<z1*z2<<endl;
     return 0;
+}
+    int comp(int a, int b){
+    int s;
+    s=a*b;
+    cout<<s<<endl;
+    return s;
 }
