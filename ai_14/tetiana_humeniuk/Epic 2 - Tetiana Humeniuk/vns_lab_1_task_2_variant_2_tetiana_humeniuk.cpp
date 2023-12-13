48ed600b6d9457d8d159a6616eefdb598ae7ff15
HumeniukTetiana <Tetiana.Humeniuk.ShI.2023@lpnu.ua>
2023-12-13 21:50:59 +0200

    bug fixed

--- ai_14/tetiana_humeniuk/Epic 2 - Tetiana Humeniuk/vns_lab_1_task_2_variant_2_tetiana_humeniuk.cpp
+++ ai_14/tetiana_humeniuk/Epic 2 - Tetiana Humeniuk/vns_lab_1_task_2_variant_2_tetiana_humeniuk.cpp
@@ -5,7 +5,7 @@ int main()
     cout<<"enter two numbers  with gap:\n";
     int n;
     int m;
-    cin>>n, m;
+    cin>>n>>m;
     cout<<(++n*++m)<<endl;
     cout<<(m++<n)<<endl;
     cout<<(n++>m);
