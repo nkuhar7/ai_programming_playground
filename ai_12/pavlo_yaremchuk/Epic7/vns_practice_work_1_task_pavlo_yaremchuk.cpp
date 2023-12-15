f2008d6ecbb0dafacdb0014022676944b1d935ab
PavloYaremchuk <pavlo.yaremchuk.shi.2023@lpnu.ua>
2023-12-15 03:08:28 +0200

    add good  rgr

--- ai_12/pavlo_yaremchuk/Epic7/vns_practice_work_1_task_pavlo_yaremchuk.cpp
+++ ai_12/pavlo_yaremchuk/Epic7/vns_practice_work_1_task_pavlo_yaremchuk.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main()
 {
+    //Задання вхідних параметрів
     double pi = M_PI;
     double x = 0.25;
     double y = 1.31;
@@ -12,12 +13,19 @@ int main()
     double b = 0.9;
     double num, denom, addend, P;
     
+    //Обчислення чисельника за вказаною формулою
     num = pow(sin(a * pow(x, 3) + b * pow(y, 2) - a * b), 3);
+
+    //Обчислення знаменника за вказаною формулою
     denom = pow(pow(a * pow(x, 3) + b * pow(y, 2) - a, 2) + pi, 1 / 3);
+
+    //Обчислення доданку за вказаною формулою
     addend = tan(a * pow(x, 3) + b * pow(y, 2) - a * b);
 
+    //Обчислення виразу P за вказаною формулою
     P = abs(num / denom) + addend;
 
+    //Виведення результату
     cout << P;
 
     return 0;
